c23f6a28dddc53642a1536c9e4dc768b02962a10
agfebrian <febriananggi9@gmail.com>
2023-04-08 12:44:30 +0700

    Fix theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      brand: "#FFC700",
-      "brand-grey": "#FAFAFC",
-    },
     extend: {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
         serif: ["Poppins", "serif"],
+      },
+      colors: {
+        brand: "#FFC700",
+        "brand-grey": "#FAFAFC",
       },
     },
   },
