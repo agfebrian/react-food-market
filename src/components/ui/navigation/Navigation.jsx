9772c1b36084ea07943cbd4287de435eb5d79561
agfebrian <febriananggi9@gmail.com>
2023-05-14 20:43:05 +0700

    Add handle navigation back

--- src/components/ui/navigation/Navigation.jsx
+++ src/components/ui/navigation/Navigation.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { Avatar } from "../avatar/Avatar";
 import IconArrowBack from "../../../assets/icons/arrow_back.svg";
 
-export const Navigation = ({ isBack, title, description, avatar }) => {
+export const Navigation = ({
+  isBack,
+  title,
+  description,
+  avatar,
+  handleBack,
+}) => {
   return (
     <div className="flex items-center gap-5 bg-white px-6 pb-6 pt-8">
-      {isBack && <img className="cursor-pointer" src={IconArrowBack} />}
+      {isBack && (
+        <img
+          className="cursor-pointer"
+          src={IconArrowBack}
+          onClick={handleBack}
+        />
+      )}
       <div className="flex flex-col">
         <h2 className="text-2xl font-medium text-black">{title}</h2>
         <p className="text-sm font-light text-brand-secondary">{description}</p>
