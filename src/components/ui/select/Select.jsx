5521bef67ce21c780b35ee497c5ca70f8d52d74e
agfebrian <febriananggi9@gmail.com>
2023-05-14 19:08:30 +0700

    Enhancement on component select

--- src/components/ui/select/Select.jsx
+++ src/components/ui/select/Select.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import clsx from "clsx";
 
-export const Select = ({ label, children, ...rest }) => {
+export const Select = ({ label, children, className, ...rest }) => {
   return (
     <div className="flex flex-col">
       <label htmlFor={label} className="mb-[6px]">
         {label}
       </label>
       <select
-        className="rounded-lg border border-black px-3 py-2 outline-none focus:border-green-300"
+        className={clsx(
+          className,
+          "rounded-lg border border-black px-3 py-2 outline-none focus:border-green-300"
+        )}
         {...rest}
       >
         {children}
