7cddcede83e68dc5130526fab53c11e229c26c35
agfebrian <febriananggi9@gmail.com>
2023-05-16 21:57:13 +0700

    Integration api auth login

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Page, Container } from "../../components/layout";
 import { Input, Button, Navigation } from "../../components/ui";
+import { setAlert } from "../../slices/alertSlice";
+import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import http from "../../app/http";
 
 export const SignIn = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const {
+    values,
+    handleChange,
+    errors,
+    touched,
+    setFieldTouched,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Email is invalid")
+        .required("Email is required"),
+      password: Yup.string().min(8).required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      setLoading(true);
+      await new Promise((r) => setTimeout(r, 1000));
+      try {
+        const {
+          status,
+          data: { data },
+        } = await http.post("/login", values);
+        console.log(data);
+        if (status === 200) {
+          dispatch(
+            setAlert({
+              show: true,
+              message: "Success signup",
+              type: "success",
+            })
+          );
+          navigate("/register/address");
+        } else {
+          dispatch(
+            setAlert({
+              show: true,
+              message: "Something went wrong",
+              type: "error",
+            })
+          );
+        }
+      } catch (error) {
+        console.log(error);
+        const {
+          response: {
+            data: {
+              meta: { message },
+            },
+          },
+        } = error;
+        dispatch(
+          setAlert({
+            show: true,
+            message: message,
+            type: "error",
+          })
+        );
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
 
   return (
     <Page>
@@ -13,19 +86,45 @@ export const SignIn = () => {
           <Navigation title="Sign In" description="Find your best ever meal" />
           <form
             autoComplete="off"
+            onSubmit={handleSubmit}
             className="mt-5 flex min-h-screen flex-col gap-4 bg-white px-6 py-7"
           >
-            <Input
-              label="Email Address"
-              type="text"
-              placeholder="Type your email address"
-            />
-            <Input
-              label="Password"
-              type="password"
-              placeholder="Type your password"
-            />
-            <Button type="submit" color="primary">
+            <div>
+              <Input
+                label="Email Address"
+                type="text"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onFocus={() => setFieldTouched("email", true)}
+                placeholder="Type your email address"
+                className={touched.email && errors.email ? "border-error" : ""}
+              />
+              {touched.email && errors.email && (
+                <p className="mt-1 text-sm text-error">{errors.email}</p>
+              )}
+            </div>
+            <div>
+              <Input
+                label="Password"
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onFocus={() => setFieldTouched("password", true)}
+                placeholder="Type your password"
+                className={
+                  touched.password && errors.password ? "border-error" : ""
+                }
+              />
+              <p className="mt-1 text-sm text-error">{errors.password}</p>
+            </div>
+            <Button
+              type="submit"
+              color="primary"
+              disabled={loading}
+              loading={loading}
+            >
               Sign In
             </Button>
             <Button
