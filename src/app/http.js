27767f8dd31e17cd2264f2307af778d5d1fd2a90
agfebrian <febriananggi9@gmail.com>
2023-06-05 00:12:41 +0700

    Fix import env vite

--- src/app/http.js
+++ src/app/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_APP_URL_API,
   headers: {
     "Content-Type": "application/json",
   },
