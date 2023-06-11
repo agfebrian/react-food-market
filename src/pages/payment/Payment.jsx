f0c0a5464d67bf5272668e42f9616fcfef92c5cf
agfebrian <febriananggi9@gmail.com>
2023-06-12 06:33:02 +0700

    Fix payment

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Page, Container, Footer } from "../../components/layout";
 import { Navigation, CardProductFlex, Button } from "../../components/ui";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/numbers";
+import { setAlert } from "../../slices/alertSlice";
+import http from "../../app/http";
 
 export const Payment = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { food, quantity } = useSelector((state) => state.payment);
   const profile = useSelector((state) => state.profile);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const total = food.price * quantity;
+  const driver = 50000;
+  const percen = 10 / 100;
+  const tax = percen * total;
+  const grossTotal = total + tax + driver;
+
+  const checkout = async () => {
+    const payload = {
+      name: profile.name,
+      email: profile.email,
+      phone: profile.phone,
+      total: grossTotal,
+      quantity: quantity,
+      food_id: food.id,
+      user_id: profile.id,
+    };
+
+    setIsLoading(true);
+    try {
+      const {
+        data: { status, data, message },
+      } = await http.post("/order", payload);
+
+      if (status) {
+        window.location.href = data.redirect_url;
+      } else {
+        dispatch(
+          setAlert({
+            status: true,
+            message: message,
+            type: "error",
+          })
+        );
+      }
+    } catch (error) {
+      dispatch(
+        setAlert({
+          status: true,
+          message: error.message,
+          type: "error",
+        })
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Page>
@@ -31,24 +82,20 @@ export const Payment = () => {
           <h2 className="mt-3 text-sm">Details Transaction</h2>
           <div className="mt-2 flex justify-between">
             <p className="text-sm text-brand-secondary">{food.name}</p>
-            <p className="text-sm text-black">
-              IDR {formatCurrency(food.price * quantity)}
-            </p>
+            <p className="text-sm text-black">IDR {formatCurrency(total)}</p>
           </div>
           <div className="mt-2 flex justify-between">
             <p className="text-sm text-brand-secondary">Driver</p>
-            <p className="text-sm text-black">IDR {formatCurrency(50000)}</p>
+            <p className="text-sm text-black">IDR {formatCurrency(driver)}</p>
           </div>
           <div className="mt-2 flex justify-between">
             <p className="text-sm text-brand-secondary">Tax 10%</p>
-            <p className="text-sm text-black">
-              IDR {formatCurrency((10 / 100) * (food.price * quantity))}
-            </p>
+            <p className="text-sm text-black">IDR {formatCurrency(tax)}</p>
           </div>
           <div className="mt-2 flex justify-between">
             <p className="text-sm text-brand-secondary">Total Price</p>
             <p className="text-sm text-blue-400">
-              IDR {formatCurrency((10 / 100) * (food.price * quantity) + 50000)}
+              IDR {formatCurrency(grossTotal)}
             </p>
           </div>
         </div>
@@ -79,7 +126,14 @@ export const Payment = () => {
       <Footer>
         <Container>
           <div className="w-full px-6 pb-3 pt-2">
-            <Button className="w-full">Checkout Now</Button>
+            <Button
+              className="w-full"
+              loading={isLoading}
+              disabled={isLoading}
+              handleClick={checkout}
+            >
+              Checkout Now
+            </Button>
           </div>
         </Container>
       </Footer>
