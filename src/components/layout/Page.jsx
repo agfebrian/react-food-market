982c34007072edc30a37fd6400d88a789898080a
agfebrian <febriananggi9@gmail.com>
2023-06-18 09:01:42 +0700

    Add prop class

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import clsx from "clsx";
 
-export const Page = ({ children }) => {
-  return <div className="w-full bg-brand-grey">{children}</div>;
+export const Page = ({ className, children }) => {
+  return (
+    <div className={clsx(className, "w-full bg-brand-grey")}>{children}</div>
+  );
 };
